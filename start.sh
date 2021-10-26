06cc1ac41defd7e72cd59164b726ebf9ffe8cbc7
m-bers <jc4gavejc@gmail.com>
2021-10-26 15:47:18 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-envsubst '$$FAVICON_URL $$APP_TITLE $$CORNER_IMAGE_URL' < /etc/nginx/nginx.tmpl > /etc/nginx/sites-enabled/default && nginx -g 'daemon off;' &
+envsubst '$$BG_GRADIENT $$FAVICON_URL $$APP_TITLE $$CORNER_IMAGE_URL' < /etc/nginx/nginx.tmpl > /etc/nginx/sites-enabled/default && nginx -g 'daemon off;' &
 nohup /usr/bin/broadwayd :5 &> /var/log/broadway.log &
 tmux new-session -d -s "ttyd" "tmux set -g status off && /bin/bash"
 ttyd tmux a -t ttyd &
